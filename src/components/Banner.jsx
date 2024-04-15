c688cb968b12f8165c8431303a3141205f089630
Raphael Sanchez <hello@raphaelsanchez.design>
2024-04-15 18:29:08 +0200

    Cleanup banner component

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,31 +2,33 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import './Banner.scss'
 
-// PropTypes
-Banner.propTypes = {
-    title: PropTypes.oneOfType([PropTypes.string, PropTypes.oneOf([null])]),
-    image: PropTypes.string.isRequired,
-}
-
 /**
  * Banner component that displays an image and an optional title.
  *
  * @component
  * @param {Object} props - The props of the component.
- * @param {string | null} props.title - The title to display. If not provided, no title will be displayed.
+ * @param {string} props.title - The title to display. If not provided, no title will be displayed.
  * @param {string} props.image - The URL of the image to display.
  * @returns {React.Element} The rendered Banner component.
- *
- * @example
- * return (
- *   <Banner title="Welcome" image="/path/to/image.jpg" />
- * )
  */
-export default function Banner({ title = null, image }) {
+export default function Banner({ title = '', image = '' }) {
     return (
         <header className="banner">
             {title && <h2 className="banner__title">{title}</h2>}
-            <img className="banner__image" src={image} alt="" />
+            {image && <img className="banner__image" src={image} alt="" />}
         </header>
     )
+}
+
+// PropTypes
+Banner.propTypes = {
+    title: PropTypes.string,
+    image: PropTypes.string,
+    children: (props, componentName) => {
+        if (!props.title && !props.image) {
+            return new Error(
+                `One of props 'title' or 'image' was not specified in '${componentName}'.`
+            )
+        }
+    },
 }
